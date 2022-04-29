3d91d814498a03a85542b78b2910df479e41a815
L-Huber <71383238+L-Huber@users.noreply.github.com>
2022-04-29 12:45:21 +0200

    created Tests for ImagePostDTO and ImagePutDTO
    -ImagePostDTOTest: created setGetImagePostDTO() which tests the class
    -ImagePutDTOTest: created setGetImagePutDTO() which tests the class

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/rest/dto/ImagePostDTOTest.java
@@ -0,0 +1,52 @@
+package ch.uzh.ifi.hase.soprafs22.rest.dto;
+
+import ch.uzh.ifi.hase.soprafs22.entity.Image;
+import ch.uzh.ifi.hase.soprafs22.entity.User;
+import ch.uzh.ifi.hase.soprafs22.rest.dto.ImagePostDTO;
+import ch.uzh.ifi.hase.soprafs22.rest.mapper.DTOMapper;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImagePostDTOTest {
+
+
+
+    @Test
+    void setGetImagePostDTO() {
+        // create test Date
+        Date date = new Date();
+        // create test User
+        User testUser = new User();
+
+        // create UserPostDTO
+        ImagePostDTO imagePostDTO = new ImagePostDTO();
+        imagePostDTO.setOwner(testUser);
+        imagePostDTO.setName("name");
+        imagePostDTO.setLocation("location");
+        imagePostDTO.setUploadDate(date);
+        imagePostDTO.setRating(0);
+        imagePostDTO.setStorageLink("storageLink");
+        imagePostDTO.setClassification("C");
+        imagePostDTO.setReachedHighlights(false);
+
+        // create image Entity via mapper
+        Image image =DTOMapper.INSTANCE.convertImagePostDTOtoEntity(imagePostDTO);
+
+        // check getters
+        assertEquals(testUser, imagePostDTO.getOwner());
+        assertEquals(image.getName(), imagePostDTO.getName());
+        assertEquals(image.getLocation(), imagePostDTO.getLocation());
+        assertEquals(date, imagePostDTO.getUploadDate());
+        assertEquals(image.getRating(), imagePostDTO.getRating());
+        assertEquals(image.getStorageLink(), imagePostDTO.getStorageLink());
+        assertEquals(image.getClassification(), imagePostDTO.getClassification());
+        assertEquals(image.getReachedHighlights(), imagePostDTO.getReachedHighlights());
+
+    }
+
+}
