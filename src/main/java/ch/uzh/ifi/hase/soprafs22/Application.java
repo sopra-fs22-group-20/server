85d42d04a60cb795b4d60a7e24aae8596bb1e4f8
JonathanContrerasM <jonathan.contreras.michael@gmail.com>
2022-05-05 18:31:26 +0200

    Added the hardcoded categories

--- src/main/java/ch/uzh/ifi/hase/soprafs22/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/Application.java
@@ -1,38 +1,66 @@
 package ch.uzh.ifi.hase.soprafs22;
 
+import ch.uzh.ifi.hase.soprafs22.entity.Category;
+import ch.uzh.ifi.hase.soprafs22.repository.CategoryRepository;
 import ch.uzh.ifi.hase.soprafs22.service.CategoryService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
 
 @RestController
 @SpringBootApplication
 public class Application {
+    private final CategoryRepository categoryRepository;
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
 
-  @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
-  @ResponseStatus(HttpStatus.OK)
-  @ResponseBody
-  public String helloWorld() {
-    return "The application is running.";
-  }
+    public Application(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
-      }
-    };
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String helloWorld() {
+        return "The application is running.";
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
+
+    @Bean
+    public void setUpCategory() {
+        Category all [] = new Category[5];
+        all[0] = new Category("Fish");
+        all[1] = new Category("Car");
+        all[2] = new Category("Cat");
+        all[3] = new Category("Dog");
+        all[4] = new Category("Motorcycle");
+
+        for (Category category : all) {
+            Category y = this.categoryRepository.save(category);
+            this.categoryRepository.flush();
+        }
+    }
 }
