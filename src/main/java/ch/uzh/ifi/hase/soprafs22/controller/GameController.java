dacbcbec673e7f0fdd400465b0c6d296b8b1f3c1
JSha91 <jamo.sharif@uzh.ch>
2022-05-22 23:51:45 +0200

    finished GameController| Game | GameRepository | GameDTO | GameService

--- src/main/java/ch/uzh/ifi/hase/soprafs22/controller/GameController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/controller/GameController.java
@@ -1,2 +1,151 @@
-package ch.uzh.ifi.hase.soprafs22.controller;public class GameController {
+package ch.uzh.ifi.hase.soprafs22.controller;
+
+import ch.uzh.ifi.hase.soprafs22.entity.Game;
+import ch.uzh.ifi.hase.soprafs22.rest.dto.GameDTO;
+import ch.uzh.ifi.hase.soprafs22.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequestMapping( "/game")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    /**
+     * Endpoints in this controller
+     * /create          POST
+     * /joinGame        POST
+     * /quit/{gameCode}/{userId} DELETE
+     * /findGameByGameCode/{gameCode}      GET
+     * /updateScore             PUT
+     * /findAllGames            GET
+     *
+     */
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createGame(@RequestBody Game game){
+        try{
+            return ResponseEntity.ok(gameService.createGame(game));
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+            return ResponseEntity.status(BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/joinGame")
+    public ResponseEntity<?> joinGame(@RequestBody Game game){
+        try{
+            return ResponseEntity.ok(gameService.joinGame(game));
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+            return ResponseEntity.status(BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/quit/{gameCode}/{userId}")
+    public ResponseEntity<?> quit(@PathVariable(name = "gameCode")UUID gameCode, @PathVariable(name = "userId") Long userId){
+        try{
+            return ResponseEntity.ok(gameService.quit(gameCode, userId));
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+            return ResponseEntity.status(BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/exitGame/{gameCode}")
+    public ResponseEntity<?> exitGame(@PathVariable(name = "gameCode")UUID gameCode){
+        try{
+            return ResponseEntity.ok(gameService.exitGame(gameCode));
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+            return ResponseEntity.status(BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/findGameByGameCode/{gameCode}")
+    public ResponseEntity<?> findGameByGameCode(@PathVariable(name = "gameCode")UUID gameCode){
+        try{
+            return ResponseEntity.ok(gameService.findGameByGameCode(gameCode));
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+            return ResponseEntity.status(BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/updateScore")
+    public ResponseEntity<?> updateScore(@RequestBody GameDTO gameDto){
+        try{
+            return ResponseEntity.ok(gameService.updateScore(gameDto));
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+            return ResponseEntity.status(BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/findAllGames")
+    public ResponseEntity<?> findAllGames(){
+        try{
+            return ResponseEntity.ok(gameService.findAllGames());
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+            return ResponseEntity.status(BAD_REQUEST).body(e.getMessage());
+        }
+    }
+    @GetMapping("/findActiveGame")
+    public ResponseEntity<?> findActiveGamme(){
+        try{
+            return ResponseEntity.ok(gameService.findActiveGame());
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+            return ResponseEntity.status(BAD_REQUEST).body(e.getMessage());
+        }
+    }
+    @GetMapping("/findGameByUserId/{user1Id}")
+    public ResponseEntity<?> findGameByUserId(@PathVariable(name = "user1Id") Long user1Id){
+        try{
+            return ResponseEntity.ok(gameService.findGameByUserId(user1Id));
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+            return ResponseEntity.status(BAD_REQUEST).body(e.getMessage());
+        }
+    }
+    @GetMapping("/checkForWinner")
+    public ResponseEntity<?> checkForWinner(){
+        try{
+            return ResponseEntity.ok(gameService.checkForWinner());
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+            return ResponseEntity.status(BAD_REQUEST).body(e.getMessage());
+        }
+    }
+    @GetMapping("/saveWinner/{id}/{gameCode}")
+    public ResponseEntity<?> saveWinner(@PathVariable(name = "id") Long id, @PathVariable(name = "gameCode") UUID gameCode){
+        try{
+            return ResponseEntity.ok(gameService.saveWinner(id, gameCode));
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+            return ResponseEntity.status(BAD_REQUEST).body(e.getMessage());
+        }
+    }
+    @GetMapping("/checkEmptyLobby")
+    public ResponseEntity<?> checkEmptyLobby(){
+        try{
+            List<Game> allGames = gameService.checkEmptyLobby();
+            if (allGames.size()>0)
+                return ResponseEntity.status(IM_USED).body("Please create a new lobby for your partner");
+            else
+                return ResponseEntity.status(ALREADY_REPORTED).body("Ruff response");
+        }catch (Exception e){
+            System.out.println("Error: "+e.getMessage());
+            return ResponseEntity.status(BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs22/service/GameService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/service/GameService.java
@@ -1,2 +1,154 @@
-package ch.uzh.ifi.hase.soprafs22.service;public class GameService {
+package ch.uzh.ifi.hase.soprafs22.service;
+
+import ch.uzh.ifi.hase.soprafs22.entity.Game;
+import ch.uzh.ifi.hase.soprafs22.repository.GameRepository;
+import ch.uzh.ifi.hase.soprafs22.rest.dto.GameDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class GameService {
+    @Autowired
+    private GameRepository  gameRepository;
+    @Autowired
+    private UserService userService;
+
+    public Game createGame(Game game) throws Exception {
+        //now create a new game
+        if (game.getUser1Id()==null)
+            throw new Exception("Please mention user id");
+        if (game.getUser1Joined()==null)
+            throw new Exception("Please mention user id");
+        game.setUser1Score(0L);
+        game.setUser1Id(game.getUser1Id());
+        game.setGameCode(UUID.randomUUID());
+        game.setActive(true);
+        return gameRepository.save(game);
+    }
+
+    public Game joinGame(Game game) throws Exception {
+        if (game.getGameCode()==null) {
+            throw new Exception("Please mention Game Code");
+        }
+        Game existingGame = gameRepository.findByGameCode(game.getGameCode()).orElseThrow(()-> new Exception("Game not found with  game code: "+game.getGameCode()));
+        if (game.getUser2Id()==null) {
+            throw new Exception("Please mention user2 id");
+        }
+        existingGame.setGameStartTime(new Date());
+        setUserNames(existingGame);
+        existingGame.setUser2Score(0L);
+        existingGame.setUser2Id(game.getUser2Id());
+        existingGame.setUser2Joined(true);
+        existingGame.setActive(true);
+        return gameRepository.save(existingGame);
+    }
+
+    private void setUserNames(Game existingGame) {
+        if (existingGame.getUser1Id()!=null)
+            existingGame.setUser1Name(userService.getUserByUserId(existingGame.getUser1Id()).getUsername());
+        if (existingGame.getUser2Id()!=null)
+            existingGame.setUser2Name(userService.getUserByUserId(existingGame.getUser2Id()).getUsername());
+    }
+
+    //to make auto rolback if any error occurs
+    @Transactional
+    public Game exitGame(UUID gameCode) throws Exception {
+        Game findGame = gameRepository.findByGameCode(gameCode).orElseThrow(()-> new Exception("Game not found for Game code = "+gameCode));
+        findGame.setActive(false);
+        return gameRepository.save(findGame);
+    }
+
+
+    public Game quit(UUID gameCode, Long userId) throws Exception {
+        if (gameCode==null) {
+            throw new Exception("Please mention Game Code");
+        }
+        Game existingGame = gameRepository.findByGameCode(gameCode).orElseThrow(()-> new Exception("Game not found with  game code: "+gameCode));
+        if (userId==null) {
+            throw new Exception("Please mention user id to quit the game");
+        }
+        if (existingGame.getUser1Id()!= userId)
+            existingGame.setWinner(existingGame.getUser1Id());
+        if (existingGame.getUser2Id()!= userId)
+            existingGame.setWinner(existingGame.getUser2Id());
+        existingGame.setActive(false);
+        setUserNames(existingGame);
+        return gameRepository.save(existingGame);
+    }
+
+    public Game findGameByGameCode(UUID gameCode) throws Exception {
+        if (gameCode==null) {
+            throw new Exception("Please mention Game Code");
+        }
+        Game game = gameRepository.findByGameCode(gameCode).orElseThrow(()-> new Exception("Game not found with  game code: "+gameCode));
+        setUserNames(game);
+        return game;
+    }
+
+    public Game updateScore(GameDTO gameDto) throws Exception {
+        try{
+            if (gameDto.getGameCode()==null)
+                throw new Exception("Please mention Game Code");
+            if (gameDto.getUserId()==null)
+                throw new Exception("Please mention User id");
+            if (gameDto.getScore1()==null && gameDto.getScore2()==null)
+                throw new Exception("Please mention Some Score");
+            Game existingGame = gameRepository.findByGameCode(gameDto.getGameCode()).orElseThrow(()-> new Exception("Game not found with  game code: "+gameDto.getGameCode()));
+            if (gameDto.getScore1()!=null)
+                existingGame.setUser1Score(existingGame.getUser1Score()+1);
+            if (gameDto.getScore2()!=null)
+                existingGame.setUser2Score(existingGame.getUser2Score()+1);
+            setUserNames(existingGame);
+            return gameRepository.save(existingGame);
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public List<Game> findAllGames() throws Exception {
+        return gameRepository.findAll();
+    }
+
+    public Game findActiveGame() throws Exception {
+        Game game= gameRepository.findActiveGame();
+        if (game==null)
+            throw new Exception("Game is not created yet, Please click on following button to create lobby first");
+        setUserNames(game);
+        return game;
+    }
+
+    public Game findGameByUserId(Long userId) throws Exception {
+        List<Game> games= gameRepository.findGameByUserId(userId);
+        if (games.isEmpty())
+            throw new Exception("Game is not created yet, Please click on following button to create lobby first");
+        if (games.get(0).getUser2Id()==null)
+            throw new Exception("Lobby is ready to join you, please click on following button to join the Lobby");
+        setUserNames(games.get(0));
+        return games.get(0);
+    }
+    public Game checkForWinner() throws Exception {
+        Game game= gameRepository.checkForLatestWinner();
+        if (game==null)
+            throw new Exception("NO WINNER EXISTS FOR NOW");
+        setUserNames(game);
+        return game;
+    }
+    public Game saveWinner(Long id, UUID gameCode) throws Exception {
+        Game game= gameRepository.findByGameCode(gameCode).orElseThrow(()-> new Exception("Game not found with  game code: "+gameCode));
+        game.setWinner(id);
+        game.setActive(false);
+        return game;
+    }
+    public List<Game> checkEmptyLobby() throws Exception {
+        List<Game> games= gameRepository.checkEmptyLobby();
+        for (Game game : games) {
+            setUserNames(game);
+        }
+        return games;
+    }
 }
